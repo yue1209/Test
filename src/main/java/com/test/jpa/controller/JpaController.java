e7fdd0e2acf6fa63e16e35ff1170d548a484bdbb
liufenming <liu.fenming@chinaott.onaliyun.com>
2018-09-29 23:46:14 +0800

    分支修改1.2

--- src/main/java/com/test/jpa/controller/JpaController.java
+++ src/main/java/com/test/jpa/controller/JpaController.java
@@ -104,7 +104,7 @@ public class JpaController {
 //        System.out.println("1a13sssss421");
 
         Timestamp timestamp = Timestamp.valueOf(LocalDateTime.MAX);
-        System.out.println("1111+test11122"+timestamp);
+        System.out.println("1111+test111222"+timestamp);
         return result;
     }
 }
