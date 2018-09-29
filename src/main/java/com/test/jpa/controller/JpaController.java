2705950b51e9664c6a2149b7406b94ed41d8abd0
liufenming <liu.fenming@chinaott.onaliyun.com>
2018-09-29 20:49:24 +0800

    分支1

--- src/main/java/com/test/jpa/controller/JpaController.java
+++ src/main/java/com/test/jpa/controller/JpaController.java
@@ -99,7 +99,7 @@ public class JpaController {
         Map<String,Object> result = new HashMap<String,Object>();
         result.put("total", all2.getNumberOfElements());
         result.put("rows",all2.getContent());
-//        System.out.println("3421");
+//        System.out.println("113421");
         return result;
     }
 }
