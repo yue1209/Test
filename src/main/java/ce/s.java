8443059aed50d5603685cfbf50c474669a286c6b
liufenming <liu.fenming@chinaott.onaliyun.com>
2018-10-18 18:12:51 +0800

    测试销毁远程文件

---
+++ src/main/java/ce/s.java
@@ -0,0 +1,13 @@
+package ce;
+
+/**
+ * @ClassName s
+ * @Author yue
+ * @Date 2018/10/18 : 18:10
+ **/
+public class s
+{
+    public static void main(String[] args) {
+        System.out.println("121");
+    }
+}
